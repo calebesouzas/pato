8ab8162e55682bd9fa837b126163c97bced7d4d3
Calebe Souza <cacalebehs@gmail.com>
2025-11-19 16:38:24 -0300

    Fixed 'maybe-uninitialized' error when compiling

--- src/pato_regex.c
+++ src/pato_regex.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char * pato_regex_parse(const char * _str) {
-    char * str = (char *)malloc(sizeof(_str) + 1);
-    for (int i = 0; i < sizeof(str); i++) {
-	printf("Character is at %c", str[i]);
+
+
+/* Function 'pato_regex_parse'
+ * Param _str is the string to be parsed, the input
+ * */
+const char * pato_regex_parse(const char * _input) {
+    char * str = (char *)malloc(sizeof(_input) + 1); // +1 for NULL in case of failure
+    if (sizeof(_input) != 0 && str != NULL) {
+	for (unsigned int i = 0; i < sizeof(str); i++) {
+	    printf("Character is at %c", (char)_input[i]);
+	}
     }
     return "";
 }
